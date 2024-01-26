83b239feda62e83cc5caba9c8350ce73c8f9fc03
leonardograut <omgleo@live.com>
2024-01-26 00:34:46 -0300

    Desempilhando elemento (pop)

--- src/pilha/Pilha.java
+++ src/pilha/Pilha.java
@@ -26,4 +26,19 @@ public class Pilha<T> extends EstruturaEstatica<T> {
      return this.elementos[tamanho-1];
 
     }
+    
+    //remove  ultimo elemento do array	
+    public T desempilha() {
+    	
+    	if (this.estaVazia()) {
+			return null;
+		}
+    	
+//    	T elemento = this.elementos[tamanho-1];
+//    	tamanho--;
+//    	
+//    	return elemento;
+    		
+    	return this.elementos[--tamanho];
+    }
 }
