adbc0a96f67f803d2ca382395b2421df7fc1ed62
leonardograut <omgleo@live.com>
2024-01-26 00:22:13 -0300

    Verificar elemento do top (peek)

--- src/teste/Aula1.java
+++ src/teste/Aula1.java
@@ -7,19 +7,12 @@ public class Aula1 {
 	public static void main(String[] args) {
 		Pilha<Integer> pilha = new Pilha<Integer>();
 		
-		 for (int i = 0; i < 11; i++) {
-		 	pilha.empilha(i);
-	 }
-        
-		//verificando se  esta vazia
-		 System.out.println(pilha.estaVazia());
-
+         System.out.println(pilha.topo());
+         System.out.println(pilha);
          pilha.empilha(1);
-
-		 System.out.println(pilha.estaVazia());
-
-
-		 System.out.println(pilha.tamanho());
+         pilha.empilha(2);
+		 System.out.println(pilha.topo());
+		 System.out.println(pilha);
 
 	}
 
