e64da68e447ebe66a46e2c3a86b6089765f55528
leonardograut <omgleo@live.com>
2024-01-29 15:36:29 -0300

    Enfileirar (adicionar) elemento (enqueue)

--- src/fila/Fila.java
+++ src/fila/Fila.java
@@ -4,12 +4,24 @@ import base.EstruturaEstatica;
 
 public class Fila<T> extends EstruturaEstatica<T> {
 
-	
+
 	public Fila() {
 		super();	
 	}
-	
+
 	public Fila(int capacidade) {
 		super(capacidade);
+	}
+
+
+	public void enfileira(T elemento) {
+		// this.elementos[this.tamanho] = elemento;
+		//	this.tamanho ++;
+
+
+
+		//this.elementos[this.tamanho++]= elemento;
+
+		this.adicionar(elemento);
 	}
 }
