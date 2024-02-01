bcaac63860fa31757347eaaf9296801be1ee8ce2
leonardograut <omgleo@live.com>
2024-02-01 15:48:53 -0300

    Percorrer a Lista

--- src/lista/ListaEncadeada.java
+++ src/lista/ListaEncadeada.java
@@ -24,7 +24,31 @@ public class ListaEncadeada<T> {
 
 	@Override
 	public String toString() {
-		return "ListaEncadeada [inicio=" + inicio + "]";
+		//[1,2,3,4]
+
+		if (this.tamanho == 0) {
+			return "[]";
+		}
+
+		StringBuilder builder = new StringBuilder("[");
+
+		No<T> atual = this.inicio;
+		
+		for (int i = 0; i <this.tamanho -1; i++) {
+			builder.append(atual.getElemento()).append(",");
+			atual = atual.getProximo();
+		}
+		
+		builder.append(atual.getElemento()).append("]");
+		
+		//mesmo exemplo com while 
+		/*builder.append(atual.getElemento()).append(",");
+		while (atual.getProximo() != null) {
+			atual = atual.getProximo();
+			builder.append(atual.getElemento()).append(",");
+		}*/
+
+		return builder.toString();
 	}
 
 
--- src/lista/ListaTeste.java
+++ src/lista/ListaTeste.java
@@ -3,19 +3,19 @@ package lista;
 public class ListaTeste {
 
 	public static void main(String[] args) {
-      
+
 		ListaEncadeada<Integer> lista = new ListaEncadeada<>();
 		lista.adiciona(1);
-		
+
 		System.out.println("Tamanho = "+lista.getTamanho());
 		System.out.println(lista);
-		
+
 		lista.adiciona(2);
 		System.out.println(lista);
-		
+
 		lista.adiciona(3);
 		System.out.println(lista);
-		
+
 		lista.adiciona(4);
 		System.out.println(lista);
 
