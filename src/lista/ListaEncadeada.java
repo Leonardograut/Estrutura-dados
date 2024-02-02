64f2364a6d84c6ddc13659bd7e1e207e99ffb75e
leonardograut <omgleo@live.com>
2024-02-01 23:45:13 -0300

    Buscar por Elemento ou Posição na Lista

--- src/lista/ListaEncadeada.java
+++ src/lista/ListaEncadeada.java
@@ -6,6 +6,9 @@ public class ListaEncadeada<T> {
 	private No<T> ultimo;
 	private int tamanho = 0;
 
+
+	private final int NAO_ENCONTRADO = -1;
+
 	public void adiciona(T elemento) {
 		No<T> celula = new No<T>(elemento);
 		if (this.tamanho == 0) {
@@ -21,11 +24,11 @@ public class ListaEncadeada<T> {
 		return this.tamanho;
 	}
 
-	
-	
+
+
 	public void limpa() {
 		for (No<T> atual = this.inicio; atual != null; ) {
-			
+
 			No<T> proximo = atual.getProximo();
 			atual.setElemento(null);
 			atual.setProximo(null);
@@ -36,6 +39,56 @@ public class ListaEncadeada<T> {
 		this.tamanho = 0;
 	}
 
+
+
+	private No<T>buscaNo(int posicao){
+
+		if (!(posicao >= 0 && posicao <= this.tamanho)) {
+			throw new IllegalArgumentException("Posicao nao existe");
+		}
+
+		No<T> noAtual = this.inicio;
+
+		for (int i = 0; i < posicao; i++) {
+			noAtual = noAtual.getProximo();
+		}
+
+		return noAtual;
+	}
+
+
+	public T buscaPorPosicao(int posicao) {
+
+
+
+		return this.buscaNo(posicao).getElemento();	
+	}
+
+
+
+
+
+	public int busca(T elemento) {
+
+		No<T> noAtual = this.inicio;
+		int pos = 0;
+		if (noAtual.getElemento().equals(elemento)) {
+
+			return pos;
+		}
+		pos++;
+
+		while (noAtual != null) {
+			noAtual = noAtual.getProximo();
+		}
+
+		return  NAO_ENCONTRADO;
+	}
+
+
+
+
+
 	@Override
 	public String toString() {
 		//[1,2,3,4]
@@ -43,18 +96,18 @@ public class ListaEncadeada<T> {
 		if (this.tamanho == 0) {
 			return "[]";
 		}
-		
+
 		StringBuilder builder = new StringBuilder("[");
 
 		No<T> atual = this.inicio;
-		
+
 		for (int i = 0; i <this.tamanho -1; i++) {
 			builder.append(atual.getElemento()).append(",");
 			atual = atual.getProximo();
 		}
-		
+
 		builder.append(atual.getElemento()).append("]");
-		
+
 		//mesmo exemplo com while 
 		/*builder.append(atual.getElemento()).append(",");
 		while (atual.getProximo() != null) {
--- src/lista/ListaTeste.java
+++ src/lista/ListaTeste.java
@@ -18,11 +18,28 @@ public class ListaTeste {
 
 		lista.adiciona(4);
 		System.out.println(lista);
-		
-		
-		lista.limpa();
-		
-		System.out.println(lista);
+
+
+		//lista.limpa();
+		//System.out.println(lista);
+
+
+		//busca por elemento
+		System.out.println("********** busca por elemento");
+		//System.out.println(lista.busca(0));
+		System.out.println(lista.busca(1));
+		System.out.println(lista.busca(2));
+		System.out.println(lista.busca(3));
+
+
+		//busca por posicao
+		System.out.println("********* busca por posicao");
+		System.out.println(lista.buscaPorPosicao(0));
+		System.out.println(lista.buscaPorPosicao(1));
+		System.out.println(lista.buscaPorPosicao(2));
+		//		System.out.println(lista.buscaPorPosicao(-1));
+		//		System.out.println(lista.buscaPorPosicao(4));
+		//		
 
 	}
 
